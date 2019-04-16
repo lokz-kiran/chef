6744e4fa6782ce05175e4d7dbe995315f979bace
lokz-kiran <48046257+lokz-kiran@users.noreply.github.com>
2019-04-16 10:37:47 +0530

    Update packages.rb

--- packages.rb
+++ packages.rb
@@ -1,17 +1,9 @@
-package 'httpd' do
 
-action :install
-
-end
-
-package 'ansible' do
-
-action :install
-
-end
-
-package 'vsftpd' do
-
-action :install
-
-end
+ yum_package("httpd") do
+   package_name "httpd"  
+   action [:install]    
+   default_guard_interpreter :default
+     declared_type :package
+    cookbook_name "(chef-apply cookbook)"
+     recipe_name "(chef-apply recipe)"
+    end
