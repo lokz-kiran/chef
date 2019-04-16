5f3753c3cf3ae638254cadae5fb999eaa81bfff1
lokz-kiran <48046257+lokz-kiran@users.noreply.github.com>
2019-04-16 10:38:58 +0530

    Delete packages.rb

--- packages.rb
+++
@@ -1,9 +0,0 @@
-
- yum_package("httpd") do
-   package_name "httpd"  
-   action [:install]    
-   default_guard_interpreter :default
-     declared_type :package
-    cookbook_name "(chef-apply cookbook)"
-     recipe_name "(chef-apply recipe)"
-    end
